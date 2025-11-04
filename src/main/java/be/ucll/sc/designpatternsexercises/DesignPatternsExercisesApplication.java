eba9d7caf9c7d187c0763dd11094a77bff6d799e
xanderdrvr <xander.deroover@student.ucll.be>
2025-11-04 11:31:33 +0100

    init

---
+++ src/main/java/be/ucll/sc/designpatternsexercises/DesignPatternsExercisesApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.sc.designpatternsexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsExercisesApplication.class, args);
+    }
+
+}
